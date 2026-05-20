a3f2fc7dd8733f2a22b8a70b7d6053ba0eb68962
Jingyi Zheng <jingyizheng6778@gmail.com>
2026-05-20 14:20:11 -0700

    Feat: Establish DB connection and user schema and todo schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  googleId: { type: String, unique: true, sparse: true },
+  email: { type: String, unique: true, required: true },
+  displayName: { type: String, required: true },
+  avatar: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", UserSchema);
